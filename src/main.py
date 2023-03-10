f1858b81163fa4361373d63e227c44a1d53616f5
Mikalai Yatsyna <nikolai.yatsyna@gmail.com>
2023-03-10 09:29:18 +0400

    Skip tls on init since running in-cluster

--- src/main.py
+++ src/main.py
@@ -9,7 +9,7 @@ VAULT_ADDR = os.getenv('VAULT_ADDR')
 SECRET_NAME = os.getenv('SECRET_NAME')
 
 if __name__ == '__main__':
-    client = hvac.Client(url=VAULT_ADDR, verify=false)
+    client = hvac.Client(url=VAULT_ADDR, verify=False)
     if client.sys.is_initialized():
         print("Vault is already initialized.")
     else:
